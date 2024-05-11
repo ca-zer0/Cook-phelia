21d0b8d9bb41d193cf417ae7f23e320f89aea15f
ca-zer0 <kokorogamochimochi@gmail.com>
2024-05-11 18:29:32 +0900

    マイグレーションファイルにバリデーション記述

--- db/migrate/20240511092548_create_recipes.rb
+++ db/migrate/20240511092548_create_recipes.rb
@@ -1,11 +1,11 @@
 class CreateRecipes < ActiveRecord::Migration[7.0]
   def change
     create_table :recipes do |t|
-      t.string :name
-      t.integer :category_id
-      t.integer :kondate_id
-      t.string :people
-      t.references :user
+      t.string :name,null: false
+      t.integer :category_id,null: false
+      t.integer :kondate_id,null: false
+      t.string :people,null: false
+      t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
