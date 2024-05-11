d6cebd9e04c3baf8ec4825f9ceac371ea0c02a59
ca-zer0 <kokorogamochimochi@gmail.com>
2024-05-11 18:28:08 +0900

    マイグレーションファイル記述

--- db/migrate/20240511092548_create_recipes.rb
+++ db/migrate/20240511092548_create_recipes.rb
@@ -1,6 +1,11 @@
 class CreateRecipes < ActiveRecord::Migration[7.0]
   def change
     create_table :recipes do |t|
+      t.string :name
+      t.integer :category_id
+      t.integer :kondate_id
+      t.string :people
+      t.references :user
 
       t.timestamps
     end
