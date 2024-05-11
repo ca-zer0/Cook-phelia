ff1ad4f8f998e71d6c087b55a29a21d722625056
ca-zer0 <kokorogamochimochi@gmail.com>
2024-05-11 18:31:26 +0900

    db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_05_11_082146) do
+ActiveRecord::Schema[7.0].define(version: 2024_05_11_092548) do
+  create_table "recipes", charset: "utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.integer "category_id", null: false
+    t.integer "kondate_id", null: false
+    t.string "people", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_recipes_on_user_id"
+  end
+
   create_table "users", charset: "utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -24,4 +35,5 @@ ActiveRecord::Schema[7.0].define(version: 2024_05_11_082146) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "recipes", "users"
 end
