47ad62811d27ed978586f74af668d553e46f9647
ca-zer0 <kokorogamochimochi@gmail.com>
2024-05-14 19:39:09 +0900

    foodモデル作成、マイグレーションバリデーション記述、db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_05_14_103102) do
+ActiveRecord::Schema[7.0].define(version: 2024_05_14_103510) do
   create_table "active_storage_attachments", charset: "utf8", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -39,6 +39,15 @@ ActiveRecord::Schema[7.0].define(version: 2024_05_14_103102) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
+  create_table "foods", charset: "utf8", force: :cascade do |t|
+    t.string "name"
+    t.string "amount"
+    t.bigint "recipe_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["recipe_id"], name: "index_foods_on_recipe_id"
+  end
+
   create_table "recipes", charset: "utf8", force: :cascade do |t|
     t.string "name", null: false
     t.integer "category_id", null: false
@@ -65,5 +74,6 @@ ActiveRecord::Schema[7.0].define(version: 2024_05_14_103102) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "foods", "recipes"
   add_foreign_key "recipes", "users"
 end
