4e955d9a39c0aa5a5d6d967fbe9577131e5f53c5
ca-zer0 <kokorogamochimochi@gmail.com>
2024-05-14 18:28:58 +0900

    コントローラーにnew,createアクション記述

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,5 +3,22 @@ class RecipesController < ApplicationController
   def index
     @recipes = Recipe.all.order("created_at DESC")
   end
+
+  def new
+    @recipe_food = Recipe.new
+  end
+
+  def create
+    recipe = Recipe.new(recipe_params)
+  
+    if recipe.save
+      params[:foods].each do |food_params|
+        recipe.foods.create(food_params.permit(:name, :amount))
+      end
+      redirect_to recipes_path, notice: 'Recipe was successfully created.'
+    else
+      render :new, alert: recipe.errors.full_messages
+    end
+  end
   
 end
