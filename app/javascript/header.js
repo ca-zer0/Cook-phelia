0a02c8cab9385129d3af577259769f7591489354
ca-zer0 <kokorogamochimochi@gmail.com>
2024-05-21 00:07:36 +0900

    クリックイベント発火で検索窓を表示するjs実装

--- app/javascript/header.js
+++ app/javascript/header.js
@@ -1,10 +1,11 @@
 function pullDown() {
-  const pullDownButton = document.getElementById("lists")
-  const pullDownLists = document.getElementById('btn')
+  const pullDownButton = document.getElementById("lists");
+  const pullDownLists = document.getElementById('btn');
   const imgTag = document.querySelector('.user-btn');
   const originalSrc = imgTag.getAttribute('src');
   const pullDownParents = document.getElementById("pull-down");
   const pullDownList = document.getElementById("down-lists");
+  const search = document.getElementById('search-btn');
 
   imgTag.addEventListener('mouseover', function() {
     this.setAttribute('src', '/assets/user2.jpg');
@@ -24,17 +25,28 @@ function pullDown() {
 
   pullDownLists.addEventListener('mouseover', function(){
     this.setAttribute("style", "background-color:#155791;")
-  })
+  });
 
   pullDownLists.addEventListener('mouseout', function(){
     this.removeAttribute("style")
-  })
+  });
 
   pullDownLists.addEventListener('click', function() {
     if (pullDownList.getAttribute("style") == "display:block;") {
       pullDownList.removeAttribute("style");
     } else {
       pullDownList.setAttribute("style", "display:block;");
+    }
+  });
+
+  search.addEventListener('click', function() {
+    var items = document.getElementsByClassName('form-items');
+    for (var i = 0; i < items.length; i++) {
+      if (items[i].style.display === 'none' || items[i].style.display === '') {
+        items[i].style.display = 'block';
+      } else {
+        items[i].style.display = 'none';
+      }
     }
   });
 };
