d77df73eb3aa56e58e7b1343536698e181133021
ca-zer0 <kokorogamochimochi@gmail.com>
2024-05-28 21:41:45 +0900

    記述ミス修正

--- spec/factories/recipes.rb
+++ spec/factories/recipes.rb
@@ -8,7 +8,7 @@ FactoryBot.define do
     association :user
 
     after(:build) do |recipe|
-      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png', content_type: 'image/png')
+      recipe.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png', content_type: 'image/png')
     end
   end
 end
