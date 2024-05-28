13a9026199f6c840b670155b4abfe945ef7b4131
ca-zer0 <kokorogamochimochi@gmail.com>
2024-05-28 13:55:33 +0900

    不要な記述削除

--- app/models/recipe_food.rb
+++ app/models/recipe_food.rb
@@ -2,14 +2,6 @@ class RecipeFood < ApplicationRecord
   include ActiveModel::Model
   attr_accessor :name, :category_id, :kondate_id,:people, :foodname, :amount, :unit, :foods
 
-  with_options presence: true do
-    validates :name
-    validates :category_id
-    validates :kondate_id
-    validates :people
-  end
-
-  has_one_attached :image
   belongs_to :recipe
   belongs_to :food
 end
