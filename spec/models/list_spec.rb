56986f39903da63da5fbb12da47efa1b41c005f8
ca-zer0 <kokorogamochimochi@gmail.com>
2024-05-31 16:53:36 +0900

    listの単体テストコード記述

--- spec/models/list_spec.rb
+++ spec/models/list_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe List, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @list = FactoryBot.build(:list)
+  end
+
+  describe '食材新規登録' do
+    context '新規登録できる場合' do
+      it "正常に登録できる" do
+        expect(@list).to be_valid
+      end
+    end
+
+    context '新規登録出来ない時' do
+      it 'recipeが紐づいていない場合は登録できない' do
+        @list.recipe = nil
+        @list.valid?
+        expect(@list.errors.full_messages).to include('must exist')
+      end
+
+      it 'userが紐づいていない場合は登録できない' do
+        @list.user = nil
+        @list.valid?
+        expect(@list.errors.full_messages).to include('must exist')
+      end
+    end
+  end
 end
