e87ed282355db997e484b3640b5edb7222277528
ca-zer0 <kokorogamochimochi@gmail.com>
2024-05-31 23:13:14 +0900

    userログインの結合テストコード追記

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe "Users", type: :system do
+RSpec.describe "user新規登録", type: :system do
   before do
     @user = FactoryBot.build(:user)
     driven_by(:selenium_chrome_headless)
@@ -22,30 +22,50 @@ RSpec.describe "Users", type: :system do
       expect(page).to have_current_path(recipes_path)
       find('.header-nav-btn').click
       expect(page).to have_selector('a', text: 'ログアウト', visible: :all)
-      expect(page).to have_no_content('新規登録')
       expect(page).to have_no_content('ログイン')
     end
   end
   context 'ユーザー新規登録ができないとき' do
     it '誤った情報ではユーザー新規登録ができずに新規登録ページへ戻ってくる' do
-      # トップページに移動する
       visit root_path
-      # トップページにサインアップページへ遷移するボタンがあることを確認する
       expect(page).to have_content('Sign up')
-      # 新規登録ページへ移動する
       visit new_user_registration_path
-      # ユーザー情報を入力する
       fill_in 'Name', with: ''
       fill_in 'Email', with: ''
       fill_in 'Password', with: ''
       fill_in 'Password confirmation', with: ''
-      # サインアップボタンを押してもユーザーモデルのカウントは上がらないことを確認する
       expect{
         find('input[name="commit"]').click
         sleep 1
       }.to change { User.count }.by(0)
-      # 新規登録ページへ戻されることを確認する
       expect(page).to have_current_path(new_user_registration_path)
+    end
+  end
+end
+
+RSpec.describe 'ログイン', type: :system do
+  before do
+    @user = FactoryBot.create(:user)
+  end
+  context 'ログインができるとき' do
+    it '保存されているユーザーの情報と合致すればログインができる' do
+      visit root_path
+      fill_in 'Email', with: @user.email
+      fill_in 'Password', with: @user.password
+      find('input[name="commit"]').click
+      expect(page).to have_current_path(recipes_path)
+      find('.header-nav-btn').click
+      expect(page).to have_selector('a', text: 'ログアウト', visible: :all)
+      expect(page).to have_no_content('ログイン')
+    end
+  end
+  context 'ログインができないとき' do
+    it '保存されているユーザーの情報と合致しないとログインができない' do
+      visit root_path
+      fill_in 'Email', with: ''
+      fill_in 'Password', with: ''
+      find('input[name="commit"]').click
+      expect(page).to have_current_path(root_path)
     end
   end
 end
