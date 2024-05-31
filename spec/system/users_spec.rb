cd9e29e3cd8860c61502ff70da113563a76f4806
ca-zer0 <kokorogamochimochi@gmail.com>
2024-05-31 16:53:49 +0900

    userの結合テスト記述

---
+++ spec/system/users_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+
+RSpec.describe "Users", type: :system do
+  before do
+    @user = FactoryBot.build(:user)
+  end
+
+  context 'ユーザー新規登録ができるとき' do 
+    it '正しい情報を入力すればユーザー新規登録ができてトップページに移動する' do
+      visit root_path
+      expect(page).to have_content('Sign up')
+      visit new_user_registration_path
+      fill_in 'Name', with: @user.name
+      fill_in 'Email', with: @user.email
+      fill_in 'Password', with: @user.password
+      fill_in 'Password confirmation', with: @user.password_confirmation
+      expect{
+        find('input[name="commit"]').click
+        sleep 1
+      }.to change { User.count }.by(1)
+      expect(page).to have_current_path(recipes_path)
+      expect(
+        find('user').find('user-nav').click
+      ).to have_content('ログアウト')
+      expect(page).to have_no_content('新規登録')
+      expect(page).to have_no_content('ログイン')
+    end
+  end
+  context 'ユーザー新規登録ができないとき' do
+    it '誤った情報ではユーザー新規登録ができずに新規登録ページへ戻ってくる' do
+      # トップページに移動する
+      # トップページにサインアップページへ遷移するボタンがあることを確認する
+      # 新規登録ページへ移動する
+      # ユーザー情報を入力する
+      # サインアップボタンを押してもユーザーモデルのカウントは上がらないことを確認する
+      # 新規登録ページへ戻されることを確認する
+    end
+  end
+end
