ef564f77fbb1753f454b4e6231949585933d9783
ca-zer0 <kokorogamochimochi@gmail.com>
2024-05-31 16:52:59 +0900

    バリデーション記述

--- app/models/list.rb
+++ app/models/list.rb
@@ -2,4 +2,9 @@ class List < ApplicationRecord
   belongs_to :user
   belongs_to :recipe
   has_many :foods
+
+  with_options presence: true do
+    validates :user
+    validates :recipe
+  end
 end
