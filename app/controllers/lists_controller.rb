c4f643a1e0c8dc84afaa7de988b9b28b325bb2ce
ca-zer0 <kokorogamochimochi@gmail.com>
2024-06-02 09:39:41 +0900

    別ユーザー情報へのアクセス制限追加

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,38 +1,42 @@
 class ListsController < ApplicationController
-
+  before_action :authenticate_user!
+  before_action :set_list, only: [:destroy]
+  
   def index
-    @lists = List.includes(:recipe).all
-    @recipe_ids = List.pluck(:recipe_id)
-    @recipes = Recipe.where(id: @recipe_ids)
+    @lists = current_user.lists.includes(:recipe)
+    @recipes = Recipe.where(id: @lists.pluck(:recipe_id))
   end
 
   def create
     @list = current_user.lists.new(list_params)
     if @list.save
-      redirect_to recipes_path
+      redirect_to lists_path, notice: 'Successfully added to the list.'
     else
       render json: @list.errors, status: :unprocessable_entity
     end
   end
 
   def destroy
-    @list = current_user.lists.find_by_recipe_id(params[:recipe_id]) 
     if @list.present?
       @list.destroy
-      flash[:notice] = "Successfully removed from the list."
+      redirect_to lists_path, notice: 'Successfully removed from the list.'
     else
-      flash[:alert] = "The item does not exist in the list."
+      redirect_to lists_path, alert: 'The item does not exist in the list.'
     end
-    redirect_to lists_path
   end
 
   def check
-    present = List.exists?(recipe_id: params[:recipe_id])
+    present = current_user.lists.exists?(recipe_id: params[:recipe_id])
     render json: { is_added: present }
   end
 
   private
+
+  def set_list
+    @list = current_user.lists.find_by(recipe_id: params[:recipe_id])
+  end
+
   def list_params
-    params.require(:list).permit(:recipe_id, :user_id)
+    params.require(:list).permit(:recipe_id)
   end
 end
